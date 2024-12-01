770c89b78f56517fd7c82e6bbafe5d953e76d4dd
PawelRa <pawelrat@poczta.onet.pl>
2024-12-01 19:49:40 +0100

    Obliczenie czasu prowadzenia

--- app.py
+++ app.py
@@ -24,13 +24,39 @@ for column in df.columns:
     if column != "ID":  # Nie dotyczy kolumny kluczowej
         df[column] = df[column].fillna(timedelta(0))
 
-# Wyświetlenie wynikowego dataframe
-# print(df_overtime)
-# print(df_overtime.applymap(type))
-# print(df_tasks)
-# print(df_tasks.applymap(type))
+# OBLICZENIA
+# Definicja limitu rocznych nadgodzin
+annual_overtime_limit = timedelta(hours=416)
 
-print(df)
+# Obliczanie czasu prowadzenia
+columns_to_sum_time_driving = ["JRJ", "PTU", "TP", "WZZ", "ZDZ", "ZT"]
+# columns_to_sum_time_driving = ["JRJ", "PTU", "WZZ", "ZDZ", "ZT"]
+# Filtracja kolumn, które faktycznie istnieją w DataFrame
+existing_time_driving_columns = [col for col in columns_to_sum_time_driving if col in df.columns]
+
+if existing_time_driving_columns:
+    # Obliczenie sumy dla istniejących kolumn
+    lead_time = df[existing_time_driving_columns].sum().sum()
+else:
+    # Jeżeli żadna z kolumn nie istnieje, ustaw czas_prowadzenia na timedelta(0)
+    lead_time = timedelta(0)
+
+print(f"Czas prowadzenia = {lead_time}")
+
+
+# Ścieżka do pliku wynikowego
+# output_path = "result.xlsx"
+
+# Konwersja kolumn timedelta na format dni dziesiętnych (kompatybilny z Excela)
+# for column in df.columns:
+#     if column != "ID":  # Pomijamy kolumnę kluczową
+#         df[column] = df[column].dt.total_seconds() / (24 * 3600)  # Konwersja timedelta na dni dziesiętne
+
+
+# Eksport do Excela z formatowaniem
+# df.to_excel(output_path, index=False)
+# print(f"Plik został zapisany jako {output_path}.")
+
 
 # Zapisanie wyników do pliku
 # output_file = DB_OUT / "combined_overtime.csv"
